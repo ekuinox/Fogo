2b79362f0a078fa7b9727622b773641c9bd52369
lm9 <depkey@me.com>
2018-11-28 22:34:34 +0900

    はい

--- FogoTest/src/main.cpp
+++ FogoTest/src/main.cpp
@@ -1,8 +1,6 @@
 #include <Fogo.h>
-#include <iostream>
 
-auto main(int argc, char ** argv) -> int
-{
+auto main(int argc, char ** argv) -> int {
 	const auto & window = Fogo::Window(800, 640);
 
 	Fogo::Window::HideConsole();
