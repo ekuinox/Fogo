a5834786bd8289977a6301062922bfe528474017
lm9 <depkey@me.com>
2018-11-29 10:22:52 +0900

    移動

--- Fogo/src/Fogo.h
+++ Fogo/src/Fogo.h
@@ -1,13 +1,3 @@
 #pragma once
 
-#include "Fogo/MappedStore.h"
-#include "Fogo/Matches.h"
-#include "Fogo/PubSub.h"
-#include "Fogo/SingletonWrapper.h"
-#include "Fogo/Subject.h"
-#include "Fogo/Time.h"
-#include "Fogo/TreeStore.h"
-#include "Fogo/TypeTraitsHelper.h"
-#include "Fogo/VectoredStore.h"
-#include "Fogo/Window.h"
-#include "Fogo/WRLTypeTraitsHelper.h"
+#include "Fogo/Utility.h"
---
+++ Fogo/src/Fogo/Utility.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#pragma once
+
+#include "./Utility/MappedStore.h"
+#include "./Utility/Matches.h"
+#include "./Utility/PubSub.h"
+#include "./Utility/SingletonWrapper.h"
+#include "./Utility/Subject.h"
+#include "./Utility/Time.h"
+#include "./Utility/TreeStore.h"
+#include "./Utility/TypeTraitsHelper.h"
+#include "./Utility/VectoredStore.h"
+#include "./Utility/Window.h"
+#include "./Utility/WRLTypeTraitsHelper.h"
