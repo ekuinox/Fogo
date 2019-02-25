e533767de5c2ea6300a64d0145782c004a220375
lm9 <depkey@me.com>
2019-02-25 15:17:53 +0900

    add comments

--- Fogo/src/Fogo/Game/Component.h
+++ Fogo/src/Fogo/Game/Component.h
@@ -15,21 +15,47 @@ namespace Fogo::Game {
 	*/
 	public:
 		const UUID uuid;
+
 		Component();
 		virtual ~Component();
 		Component(const Component &) = delete;
 		Component & operator=(const Component &) = delete;
 
+		// uuidを用いて比較する
 		bool operator==(const Component & rhs) const;
 
-		template <typename Element> void execute(const std::function<void(Element &)> func) const;
-		template <typename Element, typename Key> Utility::Result<Store::Error, Element*> get(Key key) const;
-		template <typename Element> Utility::Result<Store::Error, Handler<Element>> get(const UUID & uuid) const;
-		template <typename Element> Utility::Result<Store::Error, Handler<Element>> get() const;
-		template <typename Element = Component> Utility::Result<Store::Error, Handler<Element>> getParent() const;
-		template <typename Element, typename ... Args> Handler<Element> & create(Args ... args) const;
-		template <typename Element> Handler<Element> & bind(Element * element) const;
-		template <typename Element> void free() const;
+		// ラムダ式を使って実行する
+		template <typename Element>
+		void execute(const std::function<void(Element &)> func) const;
+
+		// インデックス用キーを用いて子インスタンスを取得する
+		template <typename Element, typename Key>
+		Utility::Result<Store::Error, Element*> get(Key key) const;
+
+		// UUIDを用いて子インスタンスのハンドラを取得する
+		template <typename Element>
+		Utility::Result<Store::Error, Handler<Element>> get(const UUID & uuid) const;
+
+		// 一つの子インスタンスのハンドラを取得する
+		// 単一であることを保証するために、0か複数の場合はエラーが返る
+		template <typename Element>
+		Utility::Result<Store::Error, Handler<Element>> get() const;
+		
+		// 親インスタンスのハンドラを取得をする
+		template <typename Element = Component>
+		Utility::Result<Store::Error, Handler<Element>> getParent() const;
+
+		// 子インスタンスを生成する
+		template <typename Element, typename ... Args>
+		Handler<Element> & create(Args ... args) const;
+
+		// 子インスタンスとしてバインドする
+		template <typename Element>
+		Handler<Element> & bind(Element * element) const;
+
+		// 子インスタンスをすべて解放する
+		template <typename Element>
+		void free() const;
 	};
 
 	template<typename Element, typename ... Args>
