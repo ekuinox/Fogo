b41c78eee9c3b5a8fa0cbe473e606cffc46915fb
lm9 <depkey@me.com>
2019-02-26 10:24:56 +0900

    ぷに...

--- FogoTest/src/Scenes/MainScene.cpp
+++ FogoTest/src/Scenes/MainScene.cpp
@@ -1,13 +1,15 @@
 #include "MainScene.h"
 #include <iostream>
 
+using Microsoft::WRL::ComPtr;
 using Fogo::Graphics::DX12::Graphics;
 using Fogo::Graphics::DX12::Texture;
+using Fogo::Graphics::DX12::FBXModel;
 using Fogo::Utility::Input;
 using Fogo::Utility::KeyCode;
 using Fogo::Utility::PubSub;
+using Fogo::Utility::MappedStore;
 using Fogo::Game::System;
-using Microsoft::WRL::ComPtr;
 using Fogo::Game::Store;
 using Fogo::Game::Component;
 using Fogo::Game::Updater;
@@ -23,55 +25,61 @@ enum class Key {
 enum class VertexShader { BOX };
 enum class PixelShader { BOX };
 enum class TextureType { BOX };
-using ResourceStore = Fogo::Utility::MappedStore<
-	VertexShader, Microsoft::WRL::ComPtr<ID3DBlob>,
-	PixelShader, Microsoft::WRL::ComPtr<ID3DBlob>,
-	TextureType, std::shared_ptr<Fogo::Graphics::DX12::Texture>
+using ResourceStore = MappedStore<
+	VertexShader, ComPtr<ID3DBlob>,
+	PixelShader, ComPtr<ID3DBlob>,
+	TextureType, std::shared_ptr<Texture>
 >;
 
-struct FBX : Component {
-	Microsoft::WRL::ComPtr<ID3DBlob> vertexShader, pixelShader;
+class Model : public Component {
 	const char * modelFile;
-	std::unique_ptr<Fogo::Graphics::DX12::FBXModel> model;
+	ComPtr<ID3DBlob> vertexShader, pixelShader;
+	std::unique_ptr<FBXModel> model;
 
-	FBX() {
-		create<Initializer>([&] {
-			model = std::make_unique<Fogo::Graphics::DX12::FBXModel>(
-				modelFile,
-				Fogo::Graphics::DX12::FBXModel::Properties().setTextureDirectory(L"./resources/Textures/")
-				.setPixelShader(pixelShader).setVertexShader(vertexShader)
-				);
-			model->matrix = DirectX::XMMatrixIdentity();
-		});
+	void initialize() {
+		model = std::make_unique<Fogo::Graphics::DX12::FBXModel>(
+			modelFile,
+			Fogo::Graphics::DX12::FBXModel::Properties().setTextureDirectory(L"./resources/Textures/")
+			.setPixelShader(pixelShader).setVertexShader(vertexShader)
+			);
+		model->matrix = DirectX::XMMatrixIdentity();
+	}
 
-		create<Updater>([&] {
-			using namespace DirectX;
-			using namespace Fogo::Utility;
-			static constexpr auto SPEED = 10.0f;
+	void update() {
+		using namespace DirectX;
+		using namespace Fogo::Utility;
+		static constexpr auto SPEED = 10.0f;
 
-			XMFLOAT3 translation{ 0, 0, 0 };
-			XMFLOAT3 rotation{ 0, 0, 0 };
+		XMFLOAT3 translation{ 0, 0, 0 };
+		XMFLOAT3 rotation{ 0, 0, 0 };
 
-			if (Input::GetPress(KeyCode::A)) rotation.y -= Time::GetElapsedTime() * SPEED;
-			if (Input::GetPress(KeyCode::D)) rotation.y += Time::GetElapsedTime() * SPEED;
-			if (Input::GetPress(KeyCode::W)) translation.z += Time::GetElapsedTime() * SPEED;
-			if (Input::GetPress(KeyCode::S)) translation.z -= Time::GetElapsedTime() * SPEED;
-			if (Input::GetPress(KeyCode::LShift)) translation.y -= Time::GetElapsedTime() * SPEED;
-			if (Input::GetPress(KeyCode::Space)) translation.y += Time::GetElapsedTime() * SPEED;
+		if (Input::GetPress(KeyCode::A)) rotation.y -= Time::GetElapsedTime() * SPEED;
+		if (Input::GetPress(KeyCode::D)) rotation.y += Time::GetElapsedTime() * SPEED;
+		if (Input::GetPress(KeyCode::W)) translation.z += Time::GetElapsedTime() * SPEED;
+		if (Input::GetPress(KeyCode::S)) translation.z -= Time::GetElapsedTime() * SPEED;
+		if (Input::GetPress(KeyCode::LShift)) translation.y -= Time::GetElapsedTime() * SPEED;
+		if (Input::GetPress(KeyCode::Space)) translation.y += Time::GetElapsedTime() * SPEED;
 
-			const auto rotation_matrix
-				= XMMatrixRotationX(XMConvertToRadians(rotation.x * Time::GetElapsedTime() * 360))
-				* XMMatrixRotationY(XMConvertToRadians(rotation.y * Time::GetElapsedTime() * 360))
-				* XMMatrixRotationZ(XMConvertToRadians(rotation.z * Time::GetElapsedTime() * 360));
+		const auto rotation_matrix
+			= XMMatrixRotationX(XMConvertToRadians(rotation.x * Time::GetElapsedTime() * 360))
+			* XMMatrixRotationY(XMConvertToRadians(rotation.y * Time::GetElapsedTime() * 360))
+			* XMMatrixRotationZ(XMConvertToRadians(rotation.z * Time::GetElapsedTime() * 360));
 
-			const auto translation_matrix = XMMatrixTranslation(translation.x, translation.y, translation.z);
+		const auto translation_matrix = XMMatrixTranslation(translation.x, translation.y, translation.z);
 
-			model->matrix = rotation_matrix * translation_matrix * model->matrix;
-		});
+		model->matrix = rotation_matrix * translation_matrix * model->matrix;
+	}
 
-		create<Renderer>([&] {
-			model->render();
-		});
+	void render() const {
+		model->render();
+	}
+
+public:
+	Model(const char * modelFile, ComPtr<ID3DBlob> vertexShader, ComPtr<ID3DBlob> pixelShader) 
+	: modelFile(modelFile), vertexShader(vertexShader), pixelShader(pixelShader) {
+		create<Initializer>([&] { initialize(); });
+		create<Updater>([&] { update(); });
+		create<Renderer>([&] { render(); });
 	}
 };
 
@@ -95,12 +103,11 @@ MainScene::MainScene() {
 		once = false;
 	}
 
-	auto & fbx = create<FBX>().makeIndex(Key::FBX1);
-
-	fbx->modelFile = "./resources/2.fbx";
-	fbx->vertexShader = ResourceStore::Get<ComPtr<ID3DBlob>>(VertexShader::BOX);
-	fbx->pixelShader = ResourceStore::Get<ComPtr<ID3DBlob>>(PixelShader::BOX);
-
+	create<Model>(
+		"./resources/2.fbx",
+		ResourceStore::Get<ComPtr<ID3DBlob>>(VertexShader::BOX),
+		ResourceStore::Get<ComPtr<ID3DBlob>>(PixelShader::BOX)
+	).makeIndex(Key::FBX1);
 
 	create<Debugger>();
 }
