3ce300d24bd0a16787e19a68bdd82310aafd3e48
lm9 <depkey@me.com>
2019-02-28 12:04:19 +0900

    ostreamに吐けるようにする

--- Fogo/src/Fogo/Debug.h
+++ Fogo/src/Fogo/Debug.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "./Debug/Console.h"
+#include "./Debug/DumpComponent.h"
--- Fogo/src/Fogo/Debug/Console.cpp
+++ Fogo/src/Fogo/Debug/Console.cpp
@@ -1,4 +1,5 @@
-#include "Console.h"
+#include "./Console.h"
+#include "./DumpComponent.h"
 #include "../Game/Components/Meta.h"
 #include <typeinfo>
 
@@ -6,24 +7,7 @@ using Fogo::Debug::Console;
 using Fogo::Game::Component;
 
 void Console::Log(Component & parent, int depth, char indentCharacter, char indentLength) {
-	std::string indent1, indent2;
-	for (auto i = 0; i < indentLength; ++i) {
-		indent1 += indentCharacter;
-	}
-	for (auto i = 0; i < depth; ++i) {
-		indent2 += indent1;
-	}
-	std::cout << indent2 << typeid(parent).name() << ": {" << std::endl;
-	std::cout << indent2 << indent1 << "uuid: " << parent.uuid << "," << std::endl;
-	if (const auto & meta = parent.get<Fogo::Game::Meta>()) {
-		std::cout << indent2 << indent1 << "name: " << meta->name << "," << std::endl;
-		std::cout << indent2 << indent1 << "tag: " << meta->tag << "," << std::endl;
-		std::cout << indent2 << indent1 << "layer: " << meta->layer << "," << std::endl;
-	}
-	parent.execute<Component>([&](Component & component) {
-		Log(component, depth + 1, indentCharacter, indentLength);
-	});
-	std::cout << indent2 << "}," << std::endl;
+	DumpComponent(std::cout, parent, depth, indentCharacter, indentLength);
 }
 
 void Console::Log(Component & parent, char indentCharacter, char indentLength) {
