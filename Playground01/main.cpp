53f9332a431fa0d114da8621ff1071a6d95de7b8
lm9 <depkey@me.com>
2019-03-09 17:46:35 +0900

    UUIDを指定してその際にeraseができるように考えている

--- Playground01/main.cpp
+++ Playground01/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <typeinfo>
+#include <unordered_map>
 #include "../Fogo/src/Fogo/Game/ContainerBase.h"
+#include "../Fogo/src/Fogo/Game/ContainetWatcher.h"
+#include "../Fogo/src/Fogo/Game/UUID.h"
 
 /*
 
@@ -8,70 +11,123 @@ C++‚Ì‹@”\‚Æ‚©‘‚¢‚Ä‚İ‚½‚¢‚â‚Â‚ª‚©‚¯‚é‚©‚È‚Ç‚¨‚ê‚ª‚·êŠ@‚»‚Ì‚P
 
 */
 
-class WatcherInterface {
+template <typename KeyType>
+class ContainerWatcherInterface {
 public:
-	virtual void watch() const = 0;
+	virtual void erase(const KeyType & uuid) const = 0;
+};
+
+template <typename Container>
+class ContainerWatcher : public ContainerWatcherInterface<typename Container::KeyType> {
+public:
+	void erase(const typename Container::KeyType & uuid) const override {
+		Container::shared.erase(uuid);
+	}
+
+	static ContainerWatcher * shared;
+};
+
+template <typename Container>
+ContainerWatcher<Container> * ContainerWatcher<Container>::shared = new ContainerWatcher<Container>();
+
+template <typename KeyType, typename Hasher = std::hash<KeyType>>
+class ContainerWatcherMaster {
+	std::unordered_map<KeyType, std::vector<ContainerWatcherInterface<KeyType>*>, Hasher> __watchers;
+
+public:
+	template <typename Container>
+	void entry(const KeyType & uuid) {
+		// ˆê“x‚«‚è‚µ‚©ŒÄ‚Î‚ê‚È‚¢‚Æ‚µ‚Ä (d•¡‚Í‚È‚¢‚à‚Ì‚Æ‚·‚é)
+		__watchers[uuid].emplace_back(ContainerWatcher<Container>::shared);
+	}
+
+	void erase(const KeyType & uuid) {
+		for (const auto & watcher : __watchers[uuid]) {
+			watcher->erase(uuid);
+		}
+		__watchers.erase(uuid);
+	}
+
+	// “o˜^‚³‚ê‚Ä‚¢‚ékey‚Ì”
+	std::size_t count() const {
+		return __watchers.size();
+	}
+
+	// Key‚É“o˜^‚³‚ê‚Ä‚¢‚éWatcher‚Ì”
+	std::size_t count(const KeyType & uuid) const {
+		try {
+			return __watchers.at(uuid).size();
+		} catch (std::out_of_range & e) {
+			return 0;
+		}
+	}
+
+	static ContainerWatcherMaster * shared;
+};
+
+template <typename KeyType, typename Hasher>
+ContainerWatcherMaster<KeyType, Hasher> * ContainerWatcherMaster<KeyType, Hasher>::shared = new ContainerWatcherMaster();
+
+template <typename T>
+struct MakeMaster {
+	static constexpr auto Correct = false;
+};
+
+template <typename ... Args>
+struct MakeMaster<Fogo::ContainerBase<Args...>> {
+	using Container = Fogo::ContainerBase<Args...>;
+	using KeyType = typename Container::KeyType;
+	using ValueType = typename Container::ValueType;
+	using Hasher = typename Container::hasher;
+	using Master = ContainerWatcherMaster<KeyType, Hasher>;
+
+	static constexpr auto Correct = true;
+
+	static void Entry(const KeyType & key) {
+		Master::shared->template entry<Container>(key);
+	}
+
+	static void Erase(const KeyType & key) {
+		Master::shared->erase(key);
+	}
+
+	static std::size_t Count() {
+		return Master::shared->count();
+	}
+
+	static std::size_t Count(const KeyType & key) {
+		return Master::shared->count(key);
+	}
+
+	Master * operator()() const {
+		return Master::shared;
+	}
 };
 
 template <typename T>
-class Watcher : public WatcherInterface {
-public:
-	void watch() const override {
-		std::cout << typeid(T).name() << " watching ’†I" << std::endl;
-		std::vector<const char *> keys {};
-		for (const auto & [key, value] : Fogo::ContainerBase<const char *, T*>::shared) {
-			if (value == nullptr) {
-				keys.emplace_back(key);
-			}
-		}
-
-		for (const auto & key : keys) {
-			Fogo::ContainerBase<const char *, T*>::shared.erase(key);
-		}
-	}
-
-	static Watcher * shared;
-};
-
-template <typename T>
-Watcher<T> * Watcher<T>::shared = nullptr;
-
-class WatcherMaster final {
-private:
-	std::vector<std::unique_ptr<WatcherInterface>> __watchers;
-
-public:
-	void watch() const {
-		for (const auto & watcher : __watchers) {
-			watcher->watch();
-		}
-	}
-
-	template <typename T>
-	void create() {
-		if (Watcher<T>::shared != nullptr) {
-			return;
-		}
-		Watcher<T>::shared = new Watcher<T>();
-		__watchers.emplace_back(Watcher<T>::shared);
-	}
-
-	static WatcherMaster * shared;
-};
-
-WatcherMaster * WatcherMaster::shared = new WatcherMaster();
+using Container = Fogo::ContainerBase<Fogo::UUID, int, Fogo::UUID::Hash>;
 
 auto main() -> int {
 
-	WatcherMaster::shared->create<int>();
-	WatcherMaster::shared->create<int>();
-	WatcherMaster::shared->create<int>();
-	WatcherMaster::shared->create<float>();
-	WatcherMaster::shared->create<float>();
-	WatcherMaster::shared->create<double>();
+	const auto uuid = Fogo::UUID();
 
-	WatcherMaster::shared->watch();
+	MakeMaster<Container<int>>::Entry(uuid);
+	MakeMaster<Container<float>>::Entry(uuid);
+	MakeMaster<Container<double>>::Entry(uuid);
+	MakeMaster<Container<int>>::Entry(Fogo::UUID());
 
+	std::cout << MakeMaster<Container<int>>::Count() << std::endl;
+
+	std::cout << MakeMaster<Container<int>>::Count(uuid) << std::endl;
+
+	std::cout << "release " << uuid << std::endl;
+	MakeMaster<Container<int>>::Erase(uuid);
+
+	std::cout << MakeMaster<Container<int>>::Count() << std::endl;
+
+	std::cout << MakeMaster<Container<int>>::Count(uuid) << std::endl;
+
+	std::cout << MakeMaster<Container<int>>::Count(Fogo::UUID());
 
 	return 0;
 }
