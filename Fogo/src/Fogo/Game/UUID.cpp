bb0db55773febf098deed58f93b2e01e91cd7522
lm9 <depkey@me.com>
2019-03-10 17:21:00 +0900

    実装を別に

---
+++ Fogo/src/Fogo/Game/UUID.cpp
@@ -0,0 +1,35 @@
+#include "./UUID.h"
+
+using namespace Fogo;
+
+std::size_t UUID::Hash::operator()(const UUID & key) const {
+	return std::hash<Engine::result_type>{}(key.raw);
+}
+
+UUID::Engine::result_type UUID::Generate() {
+	static const auto & seed = std::random_device()();
+	static Engine engine(seed);
+
+	return engine();
+}
+
+UUID::UUID() : raw(Generate()) {
+	
+}
+
+bool UUID::operator==(const UUID & rhs) const {
+	return rhs.raw == raw;
+}
+
+bool UUID::operator!=(const UUID & rhs) const {
+	return rhs.raw != raw;
+}
+
+UUID & UUID::operator=(const UUID & v) {
+	const_cast<Engine::result_type&>(raw) = v.raw;
+	return *this;
+}
+
+std::string UUID::toString() const {
+	return std::to_string(raw);
+}
--- Fogo/src/Fogo/Game/UUID.h
+++ Fogo/src/Fogo/Game/UUID.h
@@ -10,45 +10,24 @@ namespace Fogo {
 	struct UUID {
 		using Engine = std::independent_bits_engine<std::mt19937, 64, std::uint64_t>;
 
-		static auto Generate() {
-			static const auto & seed = std::random_device()();
-			static Engine engine(seed);
-
-			return engine();
-		}
+		static Engine::result_type Generate();
 
 		const Engine::result_type raw;
 
-		UUID() : raw(Generate()) {}
+		UUID();
 
 		struct Hash {
 			using result_type = std::size_t;
-
-			std::size_t operator()(const UUID & key) const {
-				return std::hash<Engine::result_type>{}(key.raw);
-			}
+			std::size_t operator()(const UUID & key) const;
 		};
 
-		bool operator==(const UUID & rhs) const {
-			return rhs.raw == raw;
-		}
-
-		bool operator!=(const UUID & rhs) const {
-			return rhs.raw != raw;
-		}
-
-		UUID & operator=(const UUID & v) {
-			const_cast<Engine::result_type&>(raw) = v.raw;
-			return * this;
-		}
-
-		std::string toString() const {
-			return std::to_string(raw);
-		}
+		bool operator==(const UUID & rhs) const;
+		bool operator!=(const UUID & rhs) const;
+		UUID & operator=(const UUID & v);
+		std::string toString() const;
 
 		friend std::ostream & operator<<(std::ostream& stream, const UUID & uuid) {
 			return stream << uuid.raw;
 		}
 	};
-
 }
