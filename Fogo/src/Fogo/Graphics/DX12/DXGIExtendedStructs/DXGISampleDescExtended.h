2ae1becf7cf05cc084897eae1b23a18b5ec123cf
ekuinox <depkey@me.com>
2019-09-13 00:53:47 +0900

    スペース追加

--- Fogo/src/Fogo/Graphics/DX12/DXGIExtendedStructs/DXGISampleDescExtended.h
+++ Fogo/src/Fogo/Graphics/DX12/DXGIExtendedStructs/DXGISampleDescExtended.h
@@ -7,7 +7,7 @@ namespace Fogo {
 	struct DXGISampleDescExtended : DXGI_SAMPLE_DESC {
 		using Super = DXGI_SAMPLE_DESC;
 		using This = DXGISampleDescExtended;
-		constexpr This(const Super& raw) : Super(raw) {}
+		constexpr This(const Super & raw) : Super(raw) {}
 		constexpr This() : Super() {}
 		constexpr auto withCount(const UINT& newCount) const->This;
 		constexpr auto withQuality(const UINT& newQuality) const->This;
